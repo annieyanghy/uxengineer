8582030b15bd41c3089fe4ad3a232cc6131faa56
annie <annieyanghy@gmail.com>
2021-02-10 17:07:07 -0500

    uxenigneer

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   $('#resume_page').click(function(){
 
-    $('#resume_page').attr("href", "Assets/Hsin-Yu Yang_Resume_2021 Winter_V2.pdf");
+    $('#resume_page').attr("href", "Assets/Hsin-Yu Yang_Resume_2021 UX Engineer_V1.pdf");
     
   })
   $('#blog_page').click(function(){
